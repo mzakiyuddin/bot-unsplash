02dd9d28a84a2b3aba7da0ea870ec979c76171dc
zakiego <belummikir@gmail.com>
2022-08-16 19:26:16 +0800

    feat: add iteration and print

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/go-rod/rod"
@@ -9,18 +10,17 @@ import (
 func main() {
 	page := rod.New().MustConnect().MustPage("https://unsplash.com/photos/1HaNBky0jxA")
 	page.MustWaitLoad()
-	page.MustElementR("span", "Download free").MustClick()
-	time.Sleep(5 * time.Second)
-	page.MustElementR("span", "Download free").MustClick()
-	time.Sleep(5 * time.Second)
-	page.MustElementR("span", "Download free").MustClick()
-	time.Sleep(5 * time.Second)
-	page.MustElementR("span", "Download free").MustClick()
-	time.Sleep(5 * time.Second)
-	page.MustElementR("span", "Download free").MustClick()
-	time.Sleep(5 * time.Second)
-	page.MustElementR("span", "Download free").MustClick()
-	time.Sleep(5 * time.Second)
-	page.MustElementR("span", "Download free").MustClick()
-	time.Sleep(5 * time.Second)
+
+	for i := 0; i < 5; i++ {
+		page.MustElementR("span", "Download free").MustClick()
+		fmt.Println("Success download")
+
+		if i == 0 {
+			time.Sleep(2 * time.Second)
+			fmt.Println("Try to click got it")
+			page.MustElementR("button", "Got it").MustClick()
+		}
+		time.Sleep(3 * time.Second)
+	}
+
 }
