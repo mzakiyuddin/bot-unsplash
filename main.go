fc9e515450ad20e29a025299b4f8796efb342342
zakiego <belummikir@gmail.com>
2022-08-16 19:39:08 +0800

    feat: add more photo

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	page := rod.New().MustConnect().MustPage("https://unsplash.com/photos/1HaNBky0jxA")
 	page.MustWaitLoad()
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 10; i++ {
 		page.MustElementR("span", "Download free").MustClick()
 		fmt.Println("Success download")
 
@@ -20,7 +20,21 @@ func main() {
 			fmt.Println("Try to click got it")
 			page.MustElementR("button", "Got it").MustClick()
 		}
-		time.Sleep(3 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 
+	page.Browser().MustPage("https://unsplash.com/photos/osVeNwhlBes")
+	page.MustWaitLoad()
+
+	for i := 0; i < 10; i++ {
+		page.MustElementR("span", "Download free").MustClick()
+		fmt.Println("Success download")
+
+		if i == 0 {
+			time.Sleep(2 * time.Second)
+			fmt.Println("Try to click got it")
+			page.MustElementR("button", "Got it").MustClick()
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
